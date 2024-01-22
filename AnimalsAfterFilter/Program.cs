e61bbdee4712cfbb2e286ae77a1f2816b4f1fcc0
Kento Ooiwa <lunatic.heros@gmail.com>
2024-01-22 16:07:22 +0900

    Filterに画像保存を追加 AfterFilterとして保存された画像を基準にフィルタを行うソフトを追加

---
+++ AnimalsAfterFilter/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalsAfterFilter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var srcPath = args.FirstOrDefault();
+            var imageDirPath = args.ElementAtOrDefault(1);
+            var dstPath = args.ElementAtOrDefault(2);
+
+            if (new[] { srcPath, imageDirPath,dstPath }.Any(string.IsNullOrEmpty))
+            {
+                Console.WriteLine("invalid args.");
+                Console.WriteLine(string.Join(Environment.NewLine, new[] { ":Usage", "AnimalsAfterFilter [srcPath] [imagePath] [dstPath]" }));
+                Environment.Exit(1);
+            }
+
+            var items = Directory.GetFiles(imageDirPath, "*.*", SearchOption.TopDirectoryOnly)
+                .Select(Path.GetFileNameWithoutExtension)
+                .Select(e =>e.Split('_'))
+                .Select(s=> (Kind: s[0], Index: int.Parse(s[1])))
+                .ToArray();
+
+            var map = new Dictionary<string, HashSet<int>   >();
+            foreach(var item in items)
+            {
+                if (!map.ContainsKey(item.Kind))
+                {
+                    map[item.Kind] = new HashSet<int>();
+                }
+
+                map[item.Kind].Add(item.Index);
+            }
+
+
+            List<string> list = new List<string>();
+            string[] lines = File.ReadAllLines(srcPath);
+            foreach ((string line, int i) in lines.Select((e, i) => (e, i)))
+            {
+                try
+                {
+                    string[] s = line.Split(',');
+                    if (map[s[0]].Contains(i))
+                    {
+                        list.Add(line);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Exception:{ex}");
+                }
+            }
+
+            File.WriteAllLines(dstPath, list);
+        }
+    }
+}
--- AnimalsFilter/Program.cs
+++ AnimalsFilter/Program.cs
@@ -7,13 +7,18 @@ using System.Threading.Tasks;
 
 namespace AnimalsFilter
 {
-    /// <summary> Program </summary>
+    /// <summary>
+    /// Program
+    /// </summary>
     internal static class Program
     {
-        /// <summary> Downloads the size of the file and get file. </summary>
-        /// <param name="url"> The URL. </param>
-        /// <returns> </returns>
-        private static async Task<long> DownloadFileAndGetFileSize(string url)
+        /// <summary>
+        /// Downloads the size of the file and get file.
+        /// </summary>
+        /// <param name="url">The URL.</param>
+        /// <param name="savePathWithoutExtension">The save path without extension.</param>
+        /// <returns></returns>
+        private static async Task<long> DownloadFileAndGetFileSize(string url,string savePathWithoutExtension)
         {
             HttpClient httpClient = HttpClientFactory.Create();
 
@@ -24,38 +29,98 @@ namespace AnimalsFilter
             if (!response.IsSuccessStatusCode)
             {
                 Console.WriteLine($"error url:{url}");
+                return -1;
             }
 
             // ファイルのサイズ取得
             long fileSize = response?.Content?.Headers?.ContentLength ?? int.MaxValue;
 
+            string fileExtension = GetFileExtension(response);
+
+            // ファイルの保存
+            string savePath = $"{savePathWithoutExtension}.{fileExtension}";
+            await SaveFile(response, savePath);
+
             Console.WriteLine($"url:{url}/size:{fileSize}");
-            return fileSize;
+            return fileSize;  
         }
 
-        /// <summary> Defines the entry point of the application. </summary>
-        /// <param name="args"> The arguments. </param>
+        /// <summary>
+        /// Saves the file.
+        /// </summary>
+        /// <param name="response">The response.</param>
+        /// <param name="savePath">The save path.</param>
+        private static async Task SaveFile(HttpResponseMessage response, string savePath)
+        {
+            using (Stream contentStream = await response.Content.ReadAsStreamAsync())
+            using (FileStream fileStream = File.Create(savePath))
+            {
+                await contentStream.CopyToAsync(fileStream);
+            }
+        }
+
+        /// <summary>
+        /// Gets the file extension.
+        /// </summary>
+        /// <param name="response">The response.</param>
+        /// <returns></returns>
+        private static string GetFileExtension(HttpResponseMessage response)
+        {
+            // Content-Typeヘッダーを取得
+            IEnumerable<string> contentTypeHeaders;
+            if (response.Content.Headers.TryGetValues("Content-Type", out contentTypeHeaders))
+            {
+                string contentType = contentTypeHeaders.FirstOrDefault();
+
+                // Content-Typeからメディアタイプを取得
+                if (!string.IsNullOrEmpty(contentType))
+                {
+                    string[] mediaTypeParts = contentType.Split('/');
+                    if (mediaTypeParts.Length == 2)
+                    {
+                        // メディアタイプのサフィックスを拡張子として使用
+                        return mediaTypeParts[1];
+                    }
+                }
+            }
+
+            return "dat";
+        }
+
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         private static async Task Main(string[] args)
         {
             string srcPath = args.FirstOrDefault();
-            int size = int.TryParse(args.ElementAtOrDefault(1), out int tmp) ? tmp : 0;
-            string dstPath = args.ElementAtOrDefault(2);
-            if (new[] { srcPath, dstPath }.Any(string.IsNullOrEmpty) || size <= 0)
+            string dstPath = args.ElementAtOrDefault(1);
+            if (new[] { srcPath, dstPath }.Any(string.IsNullOrEmpty))
             {
                 Console.WriteLine("invalid args.");
-                Console.WriteLine(string.Join(Environment.NewLine, new[] { ":Usage", "AnimalsFilter [srcPath] [size(kb)] [dstPath]" }));
+                Console.WriteLine(string.Join(Environment.NewLine, new[] { ":Usage", "AnimalsFilter [srcPath] [dstPath]" }));
                 Environment.Exit(1);
             }
 
             List<string> list = new List<string>();
             string[] lines = File.ReadAllLines(srcPath);
-            foreach (string line in lines)
+            foreach ((string line,int i) in lines.Select((e,i)=>(e,i)))
             {
-                string[] s = line.Split(',');
-                long fileSize = await DownloadFileAndGetFileSize(s[1]);
-                if (fileSize <= (size * 1024))
+                try
                 {
-                    list.Add(line);
+                    string[] s = line.Split(',');
+                    var path = Path.Combine(Path.GetDirectoryName(Path.GetFullPath(dstPath)), "image", $"{s[0]}_{i}");
+                    var saveDirPath = Path.GetDirectoryName(path);
+                    Directory.CreateDirectory(saveDirPath);
+                    long fileSize = await DownloadFileAndGetFileSize(s[1], path);
+                    if (fileSize > 0)
+                    {
+                        list.Add(line);
+                    }
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine($"Exception:{ex}");
                 }
             }
 
